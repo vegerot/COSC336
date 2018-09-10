da07f274b414a5130564320087b2008671aa809f
Max Coplan <maxcoplan@Maxs-MacBook-Pro.local>
2018-09-10 07:34:51 -0400

    printed comments

--- HW1/LongestSubsequence.java
+++ HW1/LongestSubsequence.java
@@ -1,14 +1,18 @@
+//This version is with explainations. I would advise testing it with the smallest array first. THe arrays with 50 elements takes about 3 minutes to run fully.
+
 import java.util.*;
 public class LongestSubsequence {
   
   public static void main(String[] args) {   
     Scanner input = new Scanner(System.in);
     System.out.println("Longest Subsequence\n");
-    System.out.println("Enter number of elements");
+    //user enters number of elements (integers) for the array
+    System.out.println("Enter number of elements to be placed in array:");
     int numEle = getInt(input);
     int[] list1 = new int[numEle + 1];
     System.out.println("\nEnter "+ numEle +" elements");
     int num = getInt(input);
+    //while loop allows user to enter elements seperated by a space, 999 will signal to stop loop
     while (num != 999) {
       for (int i = 1; i <= numEle; i++){
         list1[i] = num;
@@ -16,6 +20,7 @@ public class LongestSubsequence {
       }
     }
     LongestSubsequence obj = new LongestSubsequence();
+    //user entered elements are sent to 'lis' function
     int[] result = obj.lis(list1);      
     //printing values
     System.out.println("\nLength of increasing subsequence is:"+result.length);
@@ -26,6 +31,7 @@ public class LongestSubsequence {
   }
   
   //Methods
+  //getInt method validates that user is only entering integers
   public static int getInt(Scanner scan) {
     int number = 0;
     boolean done = false;
@@ -46,37 +52,121 @@ public class LongestSubsequence {
     }
     return number;
   }
-  
-  public static int[] lis(int[] array1){       
-    int number = array1.length;
+  /* 
+   for (int z = 0; z < .length;z++)
+   System.out.print( [z] + " ");
+   System.out.println();
+   */
+  //System.out.println("I am printing ");//
+  public static int[] lis(int[] array1){ 
+    System.out.println("You have entered the main function! ");//
+    int number = array1.length - 1;// minus 1 ??
     int[] array2 = new int[number];
     int[] array3 = new int[number];
     int ele = 0;
+    System.out.print("I am printing array1:");//
+    for (int z = 0; z < array1.length;z++)
+      System.out.print( array1[z] + " ");
+    System.out.println();
+    System.out.print("I am printing array2: ");//
+    for (int z = 0; z < array2.length;z++)
+      System.out.print( array2[z] + " ");
+    System.out.println();
+    System.out.print("I am printing array3: ");//
+    for (int z = 0; z < array3.length;z++)
+      System.out.print( array3[z] + " ");
+    System.out.println();
     for (int i = 1; i < number; i++) {
+      System.out.println("I am printing from for loop #1 and the iteration is: " + i);//
       int j = 0;
       //search for elements
-      for (int k = ele ; k >= 1; k--){
+      for (int k = ele/*k is starting at 0*/ ; k >= 1; k--){
+        System.out.println("I am printing from for loop #2 and the iteration is: " + k);//
         if (array1[array2[k]] < array1[i]){
+          System.out.println("You have entered the if statement of for loop #2.");//
+          System.out.print("I am printing array1[array2[k]] :" );//
+          for (int z = 0; z < array1.length;z++)
+            System.out.print( array1[z] + " ");
+          System.out.println();
+          System.out.print("I am printing array1[i] : ");//
+          for (int z = 0; z < array1.length;z++)
+            System.out.print( array1[z] + " ");
+          System.out.println();
+          System.out.println("I am printing j: " + j);
           j = k;
+          System.out.println("I am printing k: " +k);//
+          System.out.println("I am breaking from the if statement. ");//
           break;
         }
-      }           
-      array3[i] = array2[j];
-      if (j == ele || array1[i] < array1[array2[j + 1]])
-      {
-        array2[j + 1] = i;
-        //math funtion used to assign elements
-        ele = Math.max(ele,j + 1);
       }
+      System.out.println("I am back in for loop #1! ");//
+      
+      System.out.print("I am printing array3[i] :" );//
+      for (int z = 0; z < array3.length;z++)
+        System.out.print( array3[z] + " ");
+      System.out.println();
+      System.out.print("I am printing array2[j] : " );//
+      for (int z = 0; z < array2.length;z++)
+        System.out.print( array2[z] + " ");
+      System.out.println();
+      array3[i] = array2[j];
+      System.out.print("I am printing array3[i] : " );//
+      for (int z = 0; z < array3.length;z++)
+        System.out.print( array3[z] + " ");
+      System.out.println();
+      if (j == ele || array1[i] < array1[array2[j + 1]]){
+        System.out.println("You have entered the if statement of for loop #1.");//
+        System.out.println("I am printing j: " + j);//
+        System.out.println("I am printing ele: " + ele);//
+        System.out.print("I am printing array1[i] : ");//
+        for (int z = 0; z < array1.length;z++)
+          System.out.print( array1[z] + " ");
+        System.out.println();
+        System.out.print("I am printing array1[array2[j + 1]] : " );//
+        for (int z = 0; z < array2.length;z++)
+          System.out.print( array2[z] + " ");
+        System.out.println();
+        System.out.print("I am printing array2[j + 1] : ");//
+        for (int z = 0; z < array2.length;z++)
+          System.out.print( array2[z] + " ");
+        System.out.println();
+        System.out.println("I am printing i: " + i);//
+        array2[j + 1] = i;
+        System.out.print("I am printing array2[j + 1] = i : ");//
+        for (int z = 0; z < array2.length;z++)
+          System.out.print( array2[z] + " ");
+        System.out.println();
+        //math funtion used to assign elements
+        System.out.println("You have the math function!");//
+        System.out.println("I am printing ele: " + ele);//
+        ele = Math.max(ele,j + 1);
+        System.out.println("I am printing ele after the math funnction: " + ele);//
+      }
+      System.out.println("You have exited the if statement of for loop #1! ");//
     }
+    
     //backtracking process
+    System.out.println("You have exited for loop #1. ");//
+    System.out.println("You have entered the back tracking process. ");//
     int[] result = new int[ele];
+    System.out.print("I am printing result: ");//
+    for (int z = 0; z < result.length;z++)
+      System.out.print( result[z] + " ");
+    System.out.println();
     int k = array2[ele];
-    for (int i = ele - 1; i >= 0; i--)
-    {
+    System.out.println("I am printing k after k = array2[ele] : " + k);//
+    for (int i = ele - 1; i >= 0; i--){
+      System.out.println("You have entered for loop #3! ");//
+      System.out.println("I am printing result[i] : " + result[i]);//
+      System.out.println("I am printing array1[k] : "+ array1[k]);//
       result[i] = array1[k];
+      System.out.println("I am printing result[i] after result[i] = array1[k] : " + result[i]);//
+      System.out.println("I am printing k: " + k);//
       k = array3[k];
+      System.out.println("I am printing k after k = array3[k]: "+ k);//
     }
+    System.out.println("You exited for loop #3! ");//
+    System.out.println("The result will now be returned and the function will be exited! ");//
     return result;            
   }
 }
