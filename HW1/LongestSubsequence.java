6b2249e0bde171d5b7453e886a9ccc71f7b937ed
Max Coplan <maxcoplan@Maxs-MacBook-Pro.local>
2018-09-10 07:32:08 -0400

    Sage's update

--- HW1/LongestSubsequence.java
+++ HW1/LongestSubsequence.java
@@ -1,16 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package longestsubsequence;
+
+
 //import required packages
 import java.util.Scanner;
 //class LongestSubsequence
 public class LongestSubsequence
 {
-    //function
-    public int[] lis(int[] X)
+    //function, note X is the list we are running through to find the longest subsequence 
+    //P will store the location for the previous number in the final list for backtracking through the longest subsequence
+    //L stores what part of the list we have looked at so far
+    public static int[] lis(int[] X)
     {       
-        int number = X.length - 1;
-        int[] M = new int[number + 1];
-        int[] P = new int[number + 1];
+        int number = X.length;
+        int[] M = new int[number];
+        int[] P = new int[number];
         int L = 0;
-        for (int i = 1; i < number + 1; i++)
+        for (int i = 1; i < number; i++)
         {
             int j = 0;
           //search for elements
@@ -55,9 +65,31 @@ public class LongestSubsequence
         int[] result = obj.lis(arr);      
        //printing values
         System.out.print("\nLongest Subsequence is: ");
+        System.out.println("\nLength of increasing subsequence is:"+result.length);
         for (int i = 0; i < result.length; i++)
             System.out.print(result[i] +" ");
-            System.out.println("\nLength of increasing subsequence is:"+result.length);
+            
         System.out.println();
     }
 }
+/*
+Test 1
+8 in List
+10 9 2 5 3 7 101 18
+
+Test 2
+50 in list
+186  359  274  927  890  520  571  310  916  798  732  23  
+196  579  426 188  524  991  91  150  117  565  993  615  
+48  811  594  303  191  505  724  818  536  416  179  485   
+334   74  998  100  197  768  421  114  739  636  356  908   
+477  656
+
+Test 3
+50 in list
+318   536   390   598   602   408   254   868   379   565
+206   619   936   195   123   314   729   608   148   540
+256   768   404   190   559   1000   482   141   26  230
+550   881   759   122   878  350  756  82  562  897  508
+853  317   380   807   23   506   98   757   247
+*/
