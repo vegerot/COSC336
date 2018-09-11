68db58edcaa65d1f73e23b06503ec3b5929a5dcc
vegerot <mchcopl@gmail.com>
2018-09-11 10:03:04 -0400

    added comments

--- HW1/LongestSubsequence.java
+++ HW1/LongestSubsequence.java
@@ -19,30 +19,30 @@ import java.util.*;
  
  
 Please copy and paste the following Tests to test the program's functionality
-
+(copy the examples in quotation marks)
    Test 1
    8 in List
-   10 9 2 5 3 7 101 18 STOP
+   "10 9 2 5 3 7 101 18 STOP"
    Length of longest increasing subsequence is: 4
    Longest Subsequence is: 2 3 7 18 
  
    Test 2
    50 in list
-   186  359  274  927  890  520  571  310  916  798  732  23  
+   "186  359  274  927  890  520  571  310  916  798  732  23  
    196  579  426 188  524  991  91  150  117  565  993  615  
    48  811  594  303  191  505  724  818  536  416  179  485   
    334   74  998  100  197  768  421  114  739  636  356  908   
-   477  656 STOP
+   477  656 STOP"
    Length of longest increasing subsequence is: 10
    Longest Subsequence is: 186 274 310 426 524 565 594 724 739 908 
   
    Test 3
    50 in list
-   318   536   390   598   602   408   254   868   379   565
+   "318   536   390   598   602   408   254   868   379   565
    206   619   936   195   123   314   729   608   148   540
    256   768   404   190   559   1000   482   141   26  230
    550   881   759   122   878  350  756  82  562  897  508
-   853  317   380   807   23   506   98   757   247 STOP
+   853  317   380   807   23   506   98   757   247 STOP"
    Length of longest increasing subsequence is: 9
    Longest Subsequence is: 123 148 256 404 482 550 759 878 897 
   
@@ -62,10 +62,11 @@ public class LongestSubsequence
 		
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter list of numbers(enter string to stop): ");
-		//while loop allows user to enter elements seperated by a space until a string is entered
+		//user may enter input in terminal either as shell arguments (e.g. java LongestSubsequence 1 9 6 2 10) or enter during program evaluation
 		if (args.length != 0)
 			for (int i = 0; i < args.length; i++)
 				originalList.add(Integer.parseInt(args[i]));
+                //while loop allows user to enter elements seperated by a space until a string is entered (e.g. 1 9 6 2 10 STOP)
 		else while(input.hasNextInt())
 			originalList.add(input.nextInt());
 		
