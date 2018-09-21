7fb4ed2a7aca6a9d49c804e69739079ce9c3748a
Max Coplan <maxcoplan@Maxs-MacBook-Pro.local>
2018-09-21 07:15:38 -0400

    Improved file reader design

---
+++ HW2/SumParser.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
+public class SumParser
+{
+	private int[] data;
+	private int size;
+	private int target;
+
+
+	public SumParser(String filename)
+	{
+		parse(filename);
+	}
+	private void parse(String fn)
+	{
+		int[] ifFail=new int[1];
+		ifFail[0]=1;
+		try 
+		{
+
+			Scanner fileScanner=new Scanner(new File(fn));
+			size=fileScanner.nextInt();
+			target=fileScanner.nextInt();
+			data=new int[size];
+
+			for(int i=0;fileScanner.hasNextInt(); i++)
+			{
+				data[i]=fileScanner.nextInt();
+			}
+
+		} catch (FileNotFoundException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	public int[] getData()
+	{
+		return data;
+	}
+	public int getTarget()
+	{
+		return target;
+	}
+}
--- HW2/txtTester.java
+++ HW2/txtTester.java
@@ -6,10 +6,10 @@ public class txtTester
 {
 	public static void main(String[] args)
 	{
-		File file=new File("input-1.1.txt");
-		txtToArray test=new txtToArray();
-		int[] result=test.array(file);
-		for (int i=2;i<result.length;i++)
+		SumParser test=new SumParser("input-1.1.txt");
+		int[] result=test.getData();
+		int target=test.getTarget();
+		for (int i=0;i<result.length;i++)
                 {       
                         System.out.print(result[i]+" ");
                 }
