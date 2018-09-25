dc464290b74052b0263d609af89ff3382776a01f
Max Coplan <maxcoplan@maxs-mbp.towson.edu>
2018-09-25 16:32:48 -0400

    updated with better boundary conditions

--- HW2/PairsOfSums.java
+++ HW2/PairsOfSums.java
@@ -2,19 +2,39 @@ public class PairsOfSums
 {
 	public pairs(int[] nums, int t)
 	{
-		int result[]=new int[];
+		int result=0;
 		int head=0;
-		int tail=nums.length;
-		while (head>=0&&tail<=nums.length)
+		int tail=nums.length-1;
+		int headCnt=0;
+		int tailCnt=0;
+		while (head<tail)
 		{
 			if (nums[head]+nums[tail]>target)
 				tail--;
 			else if (nums[head]+nums[tail]<target)
 				head++;
-			else break;
+			else
+			{
+				result++;
+				while (nums[head+1]==nums[head]&&head!=tail)
+				{
+					headCnt++;
+					head++;
+				}
+				while (nums[tail-1]==nums[tail]&&head!=tail)
+				{
+					tailCnt++;
+					tail--;
+				}
+				
+				result+=headCnt*tailCnt;
+				head++;
+				tail--;
+
+			}
 		}
 		
-		return {head,tail};	
+		return result;	
 
 	}
 }
