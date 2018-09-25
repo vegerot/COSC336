66b2baaa94f99196d858bcf09552997758b26cb1
Max Coplan <maxcoplan@maxs-mbp.towson.edu>
2018-09-25 16:11:01 -0400

    fixed

--- HW2/PairsOfSums.java
+++ HW2/PairsOfSums.java
@@ -3,7 +3,6 @@ public class PairsOfSums
 	public pairs(int[] nums, int t)
 	{
 		int result[]=new int[];
-		for (int head=0, int tail=nums.length-1; head>=0&&tail<=nums.length; 
 		int head=0;
 		int tail=nums.length;
 		while (head>=0&&tail<=nums.length)
