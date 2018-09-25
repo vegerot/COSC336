e689753d585a32f43132d7bd99e4671a2fae3f0a
Max Coplan <maxcoplan@maxs-mbp.towson.edu>
2018-09-25 16:09:57 -0400

    first attempt

--- HW2/PairsOfSums.java
+++ HW2/PairsOfSums.java
@@ -1,7 +1,21 @@
 public class PairsOfSums
 {
-	public static void main(String[] args)
+	public pairs(int[] nums, int t)
 	{
+		int result[]=new int[];
+		for (int head=0, int tail=nums.length-1; head>=0&&tail<=nums.length; 
+		int head=0;
+		int tail=nums.length;
+		while (head>=0&&tail<=nums.length)
+		{
+			if (nums[head]+nums[tail]>target)
+				tail--;
+			else if (nums[head]+nums[tail]<target)
+				head++;
+			else break;
+		}
+		
+		return {head,tail};	
+
 	}
-	public MergeSort()
 }
