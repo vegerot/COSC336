4938e6ffa432da9b3e8dcf157b3fd28b8e990733
Sage-Isaac <43153052+Sage-Isaac@users.noreply.github.com>
2018-09-25 17:08:39 -0400

    Update mergeSortFunc.java

--- HW2/mergeSortFunc.java
+++ HW2/mergeSortFunc.java
@@ -2,17 +2,14 @@ import java.util.*;
 
 public class MergeSort {
 
-//NOT DONE
-
     public static int[] mergeSortList(int[] list, int left, int right){
-        if(right-left<=1){
-            
-                return merge(new int[]{list[left]},new int[]{list[right]});
-
-        }else{
+        if(right==left){
+            return new int[]{list[left]};
+        }
+        else{
             int middle = left+ (right-left)/2; 
             int[] sortedLeft = mergeSortList(list, left, middle);
-            int[] sortedRight = mergeSortList(list, middle, right);
+            int[] sortedRight = mergeSortList(list, middle+1, right);
             
             return merge(sortedLeft,sortedRight);
         }
@@ -23,13 +20,7 @@ public class MergeSort {
         int l=0; 
         int r=0;
         while(l<Left.length&&r<Right.length){
-            System.out.println();
-                System.out.print(merged[0]);
-                for(int i=1;i<merged.length;i++){
-                    System.out.print(", "+merged[i]);
-            }
-                System.out.println();
-            System.out.println(merged[l]+", "+merged[r]+ " "+l +" vs "+ r);
+
             if(Left[l]<Right[r]){
                 merged[l+r]=Left[l];
                 l++;
@@ -49,8 +40,9 @@ public class MergeSort {
         return merged;
     }
     
+  /* 
     public static void main(String[] args) {
-        int[] fromGetFunc = new int[] {1,2,3,4,5,6};
+        int[] fromGetFunc = new int[] {21,42,83,21,21,21};
         
         int[] sorted = mergeSortList(fromGetFunc, 0, fromGetFunc.length-1);
         
@@ -60,8 +52,5 @@ public class MergeSort {
             System.out.print(", "+sorted[i]);
         }
         System.out.println();
-    }
-    
-    
-    
+    }*/
 }
