1b2920258541457912f7197db657a3f206b5a9f7
Max Coplan <maxcoplan@maxs-mbp.towson.edu>
2018-09-25 17:27:51 -0400

    shortening

--- HW2/PairsTester.java
+++ HW2/PairsTester.java
@@ -6,14 +6,8 @@ public class PairsTester
 		SumParser inFile=new SumParser("input-1.1.txt");
 		int[] input=inFile.getData();
 		int target=inFile.getTarget();
-		for (int i=0;i<input.length;i++)
-                        System.out.print(input[i]+" ");
-		System.out.println();
 		int[] sorted=new MergeSort().mergeSortList(input);
 
-		for (int i=0; i<sorted.length;i++)
-			System.out.print(sorted[i]+" ");
-		System.out.println();
 
 		int result=new PairsOfSums().pairs(sorted,target);
 		System.out.println(result);
