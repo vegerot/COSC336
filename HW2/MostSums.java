95af35cad4c08b9af5f1e43a8b222ffdc6e4bfff
Max Coplan <maxcoplan@Maxs-MacBook-Pro.local>
2018-09-25 20:52:32 -0400

    First attemp at question 2

--- HW2/MostSums.java
+++ HW2/MostSums.java
@@ -1,4 +1,21 @@
 public class MostSums
 {
-
+	public static int getMostSums(int[] arr)
+	{
+		int Max=0;
+		int currentCounter=1;
+		for (int i=0; i<arr.length-1; i++)
+		{
+			
+			if (arr[i+1]==arr[i])
+				currentCounter++;
+			else 
+			{
+				if (currentCounter>Max)
+					Max=currentCounter;
+				currentCounter=1;
+			}
+		}
+		return Max;
+	}
 }
--- HW2/MostSumsTester.java
+++ HW2/MostSumsTester.java
@@ -2,17 +2,16 @@ public class MostSumsTester
 {
 	public static void main(String[] args)
 	{
-		SumParser test=new SumParser("input-2.1.txt");
+		SumParser test=new SumParser("input-2.6.txt");
                 int[] data=test.getData();
 
                 int[] sums=new Tuples().getSumOfPairs(data);
 		
 		sums=new MergeSort().mergeSortList(sums);
 
-                for (int i=0; i<sums.length; i++)
-                {
-                        System.out.print(sums[i]+" ");
-                }
-                System.out.println();
+		int answer=new MostSums().getMostSums(sums);
+
+		System.out.println(answer);
+
 	}
 }
