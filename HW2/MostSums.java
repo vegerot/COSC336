39356c6a6679b6b451d9a8a474e3cf4a88778472
Max Coplan <maxcoplan@maxs-mbp.towson.edu>
2018-09-26 13:41:21 -0400

    Completed number 2

--- HW2/MostSums.java
+++ HW2/MostSums.java
@@ -4,18 +4,28 @@ public class MostSums
 	{
 		int Max=0;
 		int currentCounter=1;
+		int maxTarget=0;
 		for (int i=0; i<arr.length-1; i++)
 		{
 			
 			if (arr[i+1]==arr[i])
+			{
 				currentCounter++;
+//				System.out.println(arr[i+1]+" is equal to "+arr[i]+". "+currentCounter+" in a row");
+
+			}
 			else 
 			{
+				//System.out.println(arr[i+1]+" is not equal to "+arr[i]);
 				if (currentCounter>Max)
+				{
 					Max=currentCounter;
+					maxTarget=arr[i];
+					//System.out.println("New max is "+Max);
+				}
 				currentCounter=1;
 			}
 		}
-		return Max;
+		return maxTarget;
 	}
 }
--- HW2/MostSumsTester.java
+++ HW2/MostSumsTester.java
@@ -1,17 +1,58 @@
+import java.util.Scanner;
 public class MostSumsTester
 {
 	public static void main(String[] args)
 	{
-		SumParser test=new SumParser("input-2.6.txt");
+		String file;
+		if (args.length!=0)
+			file=args[0];
+		else
+			file=getInput();
+		SumParser test=new SumParser(file);
                 int[] data=test.getData();
+		System.out.print("input data: ");	
+		printArr(data);
+
 
                 int[] sums=new Tuples().getSumOfPairs(data);
 		
+		System.out.print("possible sums: ");
+		//printArr(sums);
+		
 		sums=new MergeSort().mergeSortList(sums);
+
+                System.out.print("sorted sums: ");
+		printArr(sums);
 
 		int answer=new MostSums().getMostSums(sums);
 
-		System.out.println(answer);
+		System.out.println("The most common sum is: "+answer);
+
+	}
+
+	public static void printArr(int[] arr)
+	{
+		if (arr.length<20)
+			for (int i=0; i<arr.length; i++)
+				System.out.print(arr[i]+" ");
+		else
+		{
+			for (int i=0; i<8; i++)
+				System.out.print(arr[i]+" ");
+			System.out.print("...");
+			for (int i=arr.length-8; i<arr.length;i++)
+				System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+
+	public static String getInput()
+	{
+		System.out.print("Input file here: ");
+		Scanner cin=new Scanner(System.in);
+		String file=cin.next();
+		cin.close();
+		return file;
 
 	}
 }
