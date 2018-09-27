c9932fcf4600b8f90a0b583101258d947a1eb4c5
Max Coplan <maxcoplan@maxs-mbp.towson.edu>
2018-09-27 09:45:33 -0400

    removed debug code

--- HW2/src/MostSumsTester.java
+++ HW2/src/MostSumsTester.java
@@ -46,8 +46,8 @@ public class MostSumsTester
     
     sums=new MergeSort().mergeSortList(sums);
     
-    System.out.print("sorted sums: ");
-    printArr(sums);
+//    System.out.print("sorted sums: ");
+  //  printArr(sums);
 
     int answer=new MostSums().getMostSums(sums);
     
