8f2294afa2a58cef7c4ad7b4e62228c7f027a232
Magnhildr <43155928+Magnhildr@users.noreply.github.com>
2018-10-08 19:11:34 -0400

    Update pointPadding.java

--- HW4/src/pointPadding.java
+++ HW4/src/pointPadding.java
@@ -2,8 +2,11 @@ public class pointPadding
 {
 	public static int[] Padding(int[][] unpadded)
 	{
-		int[] padded=new int[unpadded[unpadded.length-5][0]]; //is this the right size?  I get zeroes at the end of padded
+		//int[] padded=new int[unpadded[unpadded.length-5][0]]; //is this the right size?  I get zeroes at the end of padded
                                                                       // I changed the unpadded.length to -5 and this makes no more zeros at the end of the padded
+		int[] padded=new int[unpadded[unpadded.length-1][0]]; //The zeroes at the end of padded don't make a difference to the end result
+		                                                      //Changing the unpadded.length to -5 made the program not work for the other inputs
+		                                                      //I changed it back to -1 and it worked fine across all other inputs
 		for (int u=0, p=0; u<unpadded.length-1; u++)
 		{
                 	if (unpadded[u][0]!=unpadded[u+1][0])
