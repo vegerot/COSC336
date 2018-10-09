a6b939c0f4de1c0e709b75753e9d18b99a6c298c
Sage-Isaac <43153052+Sage-Isaac@users.noreply.github.com>
2018-10-09 00:25:43 -0400

    Update pointPadding.java

--- HW4/src/pointPadding.java
+++ HW4/src/pointPadding.java
@@ -1,12 +1,14 @@
+/*This program piece converts a array of arrays into a histogram array so that the information can be fed through the given code and
+*find the largest area in a histogram fassion.
+*/
 public class pointPadding
 {
 	public static int[] Padding(int[][] unpadded)
 	{
-		//int[] padded=new int[unpadded[unpadded.length-5][0]]; //is this the right size?  I get zeroes at the end of padded
-                                                                      // I changed the unpadded.length to -5 and this makes no more zeros at the end of the padded
-		int[] padded=new int[unpadded[unpadded.length-1][0]]; //The zeroes at the end of padded don't make a difference to the end result
-		                                                      //Changing the unpadded.length to -5 made the program not work for the other inputs
-		                                                      //I changed it back to -1 and it worked fine across all other inputs
+		
+		int[] padded=new int[unpadded[unpadded.length-1][0]];  	// makes it so the last X coordinate is the highest value of the 
+									// new histogram array as their is no backtracking in the data sets
+		
 		for (int u=0, p=0; u<unpadded.length-1; u++)
 		{
                 	if (unpadded[u][0]!=unpadded[u+1][0])
