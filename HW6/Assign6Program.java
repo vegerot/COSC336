dd034ea0eb71cf6ba3ae52d505ea103b77adf6ea
Sage-Isaac <43153052+Sage-Isaac@users.noreply.github.com>
2018-11-06 16:34:41 -0500

    Update Assign6Program.java

--- HW6/Assign6Program.java
+++ HW6/Assign6Program.java
@@ -1,4 +1,5 @@
 
+
 package assign.pkg6.program;
 
 
@@ -8,7 +9,6 @@ public class Assign6Program {
     
     
     public static void main(String[] args) {
-        int[] list = new int[]{5, 13, 0, 0, 56, 0, 13, 0, 0, 0};
         int[] list  = new int[]{5, 13, 0, 0, 56, 0, 13, 0, 0, 0};
         
         //initializing to the answer "no"
@@ -17,18 +17,40 @@ public class Assign6Program {
         
        randomSelect(list);
     }
-	public static void randomSelect(int[] list){
-		randomSelect(list, list.length, 0)
-	
-	}
-    
+    public static void randomSelect(int[] list){
+        randomSelect(list, 0,list.length);
+
+    }
+        
     public static void randomSelect(int[] fullList, int leftBound, int rightBound){
-        int pivot = (int)(fullList.length*Math.random());
-        int holder = fullList[pivot];
-        fullList[pivot]=fullList[0];
-        fullList[0]=fullList[holder];
+        int pivot = (int)(fullList.length*Math.random()); 
+        //index of pivot at this point not actual pivot
         
+        fullList[pivot]+=fullList[leftBound]; 
+        fullList[leftBound]=fullList[pivot]-fullList[leftBound];
+        fullList[pivot]-=fullList[leftBound];
+        //swaps the pivot with the first element of the group in question
         
+        pivot=fullList[leftBound]; 
+        //set pivot to actual pivot value not index of pivot (space efficiency)
+        
+        int l=0;
+        int r=0;
+        while(leftBound+l<=rightBound-r){
+            while(leftBound+l<=pivot){
+                l++;
+            }
+            while(rightBound-r>pivot){
+                r++;
+            }
+        if(leftBound+l<=rightBound-r){
+            fullList[leftBound+l]+=fullList[]; 
+        fullList[]=fullList[]-fullList[];
+        fullList[leftBound+l]-=fullList[];
+        }
+        
+            
+        }
         
     }
 }
