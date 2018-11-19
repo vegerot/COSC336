76567ff5419ff66fee0b4d957acd11665f749de2
Sage-Isaac <43153052+Sage-Isaac@users.noreply.github.com>
2018-11-18 19:40:51 -0500

    Update ShortestPath.java

--- HW7/ShortestPath.java
+++ HW7/ShortestPath.java
@@ -1,10 +1,24 @@
 public class ShortestPath
 {
-	ShortestPath(Graph graph,int start)
+	ShortestPath(Graph graph,int start, int target)
 	{
+		int numNodesLeft= /*(find number of nodes and put here)*/;
 		Stack s= new Stack;
 		s.push(v);
-		while(!
+		numNodesLeft --;
+		while(!v.visited && numNodesLeft>0){
+			v.visted=true;
+			count ++;
+			if(v =target){
+				return count;	
+			}
+			ArrayList<Node> children =v.getChildren()
+			for(int i=0;i<children.Size();i++){
+				s.push(children<i>);
+			}
+			s.pop(v)
+			
+		}
 
 	}
 }
