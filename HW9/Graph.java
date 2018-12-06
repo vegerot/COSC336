97d86f6d23c54254fcfe2dccd547a782688c729e
Sage-Isaac <43153052+Sage-Isaac@users.noreply.github.com>
2018-12-05 21:42:17 -0500

    Update Graph.java
    minor comments

--- HW9/Graph.java
+++ HW9/Graph.java
@@ -80,18 +80,7 @@ class Graph
         //   printArr(dist,V); 
 		} 
 
-		// Step 3: check for negative-weight cycles. The above 
-		// step guarantees shortest distances if graph doesn't 
-		// contain negative weight cycle. If we get a shorter 
-		// path, then there is a cycle. 
-		/*for (int j=0; j<E; j++) 
-		{ 
-			int u = graph.edge[j].src; 
-			int v = graph.edge[j].dest; 
-			int weight = graph.edge[j].weight; 
-			if (Dist[u]!=999&&dist[u]+weight < dist[v]) 
-			System.out.println("Graph contains negative weight cycle"); 
-		}*/
+		
        System.out.println("FINAL"); 
 		printArr(newDist, V); 
 	} 
@@ -109,8 +98,8 @@ class Graph
         int previousDist[] = new int[V];
         int newDist[] = new int[V];
         int prev[] = new int[V];
-        //if (k>V) //would k>V or k>E ????
-           // k=V;
+        if (k>V) 
+           k=V;
         System.out.println("\nk= "+k);
 
         // Step 1: Initialize distances from src to all other
@@ -124,9 +113,9 @@ class Graph
         previousDist[src] = 0;
         newDist[src]=0;
 
-        // Step 2: Relax all edges k times. A simple
+        // Step 2: Relax all edges at most k times. A simple
         // shortest path from src to any other vertex can
-        // have at-most |V| - 1 edges
+        // have at-most |V| - 1 edges so set k to v when k was greater than V
         for (int i=1; i<k+1; i++)
         {
             for (int v=0; v<V; v++)
@@ -150,9 +139,7 @@ class Graph
            //printArr(newDist,V);
         
 
- /*System.out.println("\nVertex  Distance from Source  Previous");
-        for (int m=0; m<V; m++)
-            System.out.println(dest+"\t\t"+newDist[dest]+"\t\t"+prev[dest]);*/
+ 
         }
         
             System.out.println("Start\t\tend\t\tlength");
@@ -163,75 +150,6 @@ class Graph
 
     } 
 
-	// Driver method to test above function 
-	public static void main(String[] args) 
-	{ 
-		int V = 6; // Number of vertices in graph 
-		int E = 10; // Number of edges in graph 
-
-        int graph[][] = new int[][]{ {0,f,f,f,-1,f},
-                                     {1,0,f,2,f,f},
-                                     {0,2,0,f,f,-8},
-                                     {-4,f,f,0,3,f},
-                                     {f,7,f,f,0,f},
-                                     {f,5,10,f,f,0}
-                                   };
-//        shortestPath(graph, 0,1,V,E);
-
-		/*Graph graph = new Graph(V, E); 
-
-		// add edge 0-1 (or A-B in above figure) 
-		graph.edge[0].src = 0; 
-		graph.edge[0].dest = 4; 
-		graph.edge[0].weight = -1; 
-
-		// add edge 0-2 (or A-C in above figure) 
-		graph.edge[1].src = 1; 
-		graph.edge[1].dest = 3; 
-		graph.edge[1].weight = 2; 
-
-		// add edge 1-2 (or B-C in above figure) 
-		graph.edge[2].src = 1; 
-		graph.edge[2].dest = 0; 
-		graph.edge[2].weight = 1; 
-
-		// add edge 1-3 (or B-D in above figure) 
-		graph.edge[3].src = 2; 
-		graph.edge[3].dest = 1; 
-		graph.edge[3].weight = 2; 
-
-		// add edge 1-4 (or A-E in above figure) 
-		graph.edge[4].src = 2; 
-		graph.edge[4].dest = 5; 
-		graph.edge[4].weight = -8; 
-
-		// add edge 3-2 (or D-C in above figure) 
-		graph.edge[5].src = 3; 
-		graph.edge[5].dest = 0; 
-		graph.edge[5].weight = -4; 
-
-		// add edge 3-1 (or D-B in above figure) 
-		graph.edge[6].src = 3; 
-		graph.edge[6].dest = 4; 
-		graph.edge[6].weight = 3; 
-
-		// add edge 4-3 (or E-D in above figure) 
-		graph.edge[7].src = 4; 
-		graph.edge[7].dest = 1; 
-		graph.edge[7].weight = 7; 
-
-		// add edge 4-3 (or E-D in above figure) 
-		graph.edge[8].src = 5; 
-		graph.edge[8].dest = 1; 
-		graph.edge[8].weight = 5; 
-
-		// add edge 4-3 (or E-D in above figure) 
-		graph.edge[9].src = 5; 
-		graph.edge[9].dest = 2; 
-		graph.edge[9].weight = 10; */
-
-//		graph.modifiedBellmanFord(graph, 0,3); 
-	} 
 } 
 // Inspired by Aakash Hasija code on Geeks For Geeks
 
