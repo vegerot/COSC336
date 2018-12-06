18b1c2780a8f2da12d44535627367a10094ca27a
Sage-Isaac <43153052+Sage-Isaac@users.noreply.github.com>
2018-12-05 21:27:43 -0500

    Update Graph.java

--- HW9/Graph.java
+++ HW9/Graph.java
@@ -1,3 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
 // A Java program for Bellman-Ford's single source shortest path 
 // algorithm. 
 import java.util.*; 
@@ -156,8 +162,11 @@ class Graph
         }
 
         System.out.println("Start\t\tend\t\tlength");
-        System.out.println(src+"\t\t"+dest+"\t\t"+newDist[dest]);
-
+        if(newDist[dest]==f){
+            System.out.println((src+1)+"\t\t"+(1+dest)+"\t\t"+"Cant be reached");
+        }else{
+            System.out.println((src+1)+"\t\t"+(1+dest)+"\t\t"+newDist[dest]);
+        }
     } 
 
 	// Driver method to test above function 
@@ -176,52 +185,42 @@ class Graph
 //        shortestPath(graph, 0,1,V,E);
 
 		/*Graph graph = new Graph(V, E); 
-
 		// add edge 0-1 (or A-B in above figure) 
 		graph.edge[0].src = 0; 
 		graph.edge[0].dest = 4; 
 		graph.edge[0].weight = -1; 
-
 		// add edge 0-2 (or A-C in above figure) 
 		graph.edge[1].src = 1; 
 		graph.edge[1].dest = 3; 
 		graph.edge[1].weight = 2; 
-
 		// add edge 1-2 (or B-C in above figure) 
 		graph.edge[2].src = 1; 
 		graph.edge[2].dest = 0; 
 		graph.edge[2].weight = 1; 
-
 		// add edge 1-3 (or B-D in above figure) 
 		graph.edge[3].src = 2; 
 		graph.edge[3].dest = 1; 
 		graph.edge[3].weight = 2; 
-
 		// add edge 1-4 (or A-E in above figure) 
 		graph.edge[4].src = 2; 
 		graph.edge[4].dest = 5; 
 		graph.edge[4].weight = -8; 
-
 		// add edge 3-2 (or D-C in above figure) 
 		graph.edge[5].src = 3; 
 		graph.edge[5].dest = 0; 
 		graph.edge[5].weight = -4; 
-
 		// add edge 3-1 (or D-B in above figure) 
 		graph.edge[6].src = 3; 
 		graph.edge[6].dest = 4; 
 		graph.edge[6].weight = 3; 
-
 		// add edge 4-3 (or E-D in above figure) 
 		graph.edge[7].src = 4; 
 		graph.edge[7].dest = 1; 
 		graph.edge[7].weight = 7; 
-
 		// add edge 4-3 (or E-D in above figure) 
 		graph.edge[8].src = 5; 
 		graph.edge[8].dest = 1; 
 		graph.edge[8].weight = 5; 
-
 		// add edge 4-3 (or E-D in above figure) 
 		graph.edge[9].src = 5; 
 		graph.edge[9].dest = 2; 
@@ -231,5 +230,4 @@ class Graph
 	} 
 } 
 // Contributed by Aakash Hasija 
-
 
